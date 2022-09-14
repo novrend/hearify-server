ae0849a01a574784e1447994b0fd466819f09468
ihza <ihzan0@gmail.com>
2022-09-14 23:13:26 +0700

    feat: file upload with mutler

--- routes/playlist.js
+++ routes/playlist.js
@@ -1,6 +1,32 @@
 const router = require("express").Router();
 const playlistController = require("../controllers/playlistController");
 const { isLoggedIn, isAuthorized } = require("../middlewares/auth");
+const multer = require("multer");
+const diskStorage = require("../helpers/multer");
+
+const filePhoto = multer({
+  storage: diskStorage,
+  fileFilter: (req, file, cb) => {
+    const fileSize = parseInt(req.headers["content-length"]);
+
+    if (
+      file.mimetype == "image/png" ||
+      file.mimetype == "image/jpg" ||
+      file.mimetype == "image/jpeg"
+    ) {
+      cb(null, true);
+    } else if (fileSize > 1048576 / 2) {
+      return cb({ code: 400, msg: "File size too big" });
+    } else {
+      cb(null, false);
+      return cb({ code: 400, msg: "File format not allowed" });
+    }
+  },
+  limits: {
+    fieldNameSize: 64,
+    fileSize: 1048576 / 2,
+  },
+}).single("img");
 
 router.get("/", playlistController.getPlaylist);
 
