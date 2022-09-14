d46cbf9273e1d9f2aacd2ed3b971b70ca70acf5d
ihza <ihzan0@gmail.com>
2022-09-14 23:05:54 +0700

    qris payment gateway xendit

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -82,6 +82,101 @@ class Controller {
       next(err);
     }
   }
+
+  static async createTrxQRIS(req, res, next) {
+    try {
+      const findTrx = await Transaction.findOne({
+        where: {
+          UserId: req.user.id,
+        },
+        include: {
+          model: User,
+        },
+      });
+      if (findTrx?.User?.isPremium) {
+        throw { code: 400, msg: "User already premium" };
+      }
+      const access_token = signJWT({
+        id: req.user.id,
+      });
+      const trxId = Date.now().toString();
+      const response = await axios.post(
+        "https://api.xendit.co/qr_codes",
+        {
+          external_id: trxId,
+          reference_id: trxId,
+          currency: "IDR",
+          type: "DYNAMIC",
+          callback_url: `${process.env.BASE_URL_SERVER}/transaction/paid?id=${trxId}&jwt=${access_token}`,
+          amount: 1,
+        },
+        {
+          headers: {
+            authorization: `Basic ${process.env.XENDIT_BASE64}`,
+            "content-type": "application/json",
+          },
+        }
+      );
+      if (findTrx) {
+        await Transaction.update(
+          {
+            transactionId: trxId,
+            paymentGateway: "qris",
+            url: response.data.callback_url,
+          },
+          {
+            where: {
+              id: findTrx.id,
+            },
+          }
+        );
+      } else {
+        await Transaction.create({
+          transactionId: trxId,
+          paymentGateway: "qris",
+          url: response.data.callback_url,
+          UserId: req.user.id,
+        });
+      }
+      res.status(201).json({
+        paymentGateway: "qris",
+        url: response.data.callback_url,
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  static async paidTransaction(req, res, next) {
+    try {
+      const { id, jwt } = req.query;
+      const user = verifyJWT(jwt);
+      const findTrx = await Transaction.findOne({
+        where: {
+          transactionId: id,
+          UserId: user.id,
+        },
+      });
+      if (!findTrx) {
+        throw { code: 404, msg: "Transaction not found" };
+      }
+      await User.update(
+        {
+          premium: true,
+        },
+        {
+          where: {
+            id: user.id,
+          },
+        }
+      );
+      res.status(200).json({
+        message: "Payment successfully paid",
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
 }
 
 module.exports = Controller;
--- routes/transaction.js
+++ routes/transaction.js
@@ -4,5 +4,7 @@ const { checkLogin } = require("../middlewares/auth");
 
 router.get("/me", checkLogin, transactionController.me);
 router.get("/dana", checkLogin, transactionController.createTrxDana);
+router.get("/qris", checkLogin, transactionController.createTrxQRIS);
+router.get("/paid", transactionController.paidTransaction);
 
 module.exports = router;
