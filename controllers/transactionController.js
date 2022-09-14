2cfca464e7be737adaea859e2e0aa8dd819d6953
ihza <ihzan0@gmail.com>
2022-09-14 23:05:03 +0700

    dana payment gateway xendit

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -16,6 +16,72 @@ class Controller {
       next(err);
     }
   }
+
+  static async createTrxDana(req, res, next) {
+    try {
+      const findTrx = await Transaction.findOne({
+        where: {
+          UserId: req.user.id,
+        },
+        include: {
+          model: User,
+        },
+      });
+      if (findTrx?.User?.isPremium) {
+        throw { code: 400, msg: "User already premium" };
+      }
+      const access_token = signJWT({
+        id: req.user.id,
+      });
+      const trxId = Date.now().toString();
+      const response = await axios.post(
+        "https://api.xendit.co/ewallets/charges",
+        {
+          reference_id: trxId,
+          currency: "IDR",
+          amount: 1,
+          checkout_method: "ONE_TIME_PAYMENT",
+          channel_code: "ID_DANA",
+          channel_properties: {
+            success_redirect_url: `${process.env.BASE_URL_SERVER}/transaction/paid?id=${trxId}&jwt=${access_token}`,
+          },
+        },
+        {
+          headers: {
+            authorization: `Basic ${process.env.XENDIT_BASE64}`,
+            "content-type": "application/json",
+          },
+        }
+      );
+      if (findTrx) {
+        await Transaction.update(
+          {
+            transactionId: trxId,
+            paymentGateway: "dana",
+            url: response.data.actions.desktop_web_checkout_url,
+          },
+          {
+            where: {
+              id: findTrx.id,
+            },
+          }
+        );
+      } else {
+        await Transaction.create({
+          transactionId: trxId,
+          paymentGateway: "dana",
+          url: response.data.actions.desktop_web_checkout_url,
+          UserId: req.user.id,
+        });
+      }
+      res.status(201).json({
+        paymentGateway: "dana",
+        url: response.data.actions.desktop_web_checkout_url,
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
 }
 
 module.exports = Controller;
--- routes/transaction.js
+++ routes/transaction.js
@@ -3,5 +3,6 @@ const transactionController = require("../controllers/transactionController");
 const { checkLogin } = require("../middlewares/auth");
 
 router.get("/me", checkLogin, transactionController.me);
+router.get("/dana", checkLogin, transactionController.createTrxDana);
 
 module.exports = router;
